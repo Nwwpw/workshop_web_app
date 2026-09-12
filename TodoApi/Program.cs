fa060b7cb7367e2874f1ee9ce03b988952762a5b
Nwwpw <wipawin.t@ku.th>
2026-09-12 17:15:37 +0700

    [dev] add GET endpoint filter by id

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,24 +1,35 @@
+using TodoApi.Dtos;
+ 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+ 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+ 
 app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Hello Todo API");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+ 
+var todos = new List<TodoGetDto>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+  new(1, "Learn C#", true),
+  new(2, "Lern ASP .NET Core", false),
+  new(3, "Build a web API", false),
+};
+ 
+app.MapGet("/api/todos", ()=> Results.Ok(todos));
+ 
+app.MapGet("/api/todoes/{id}", (int id) =>
+{
+    var todo = todos.FirstOrDefault(t => t.Id == id);
+
+    return todo is not null ? Results.Ok(todo) : Results.NotFound();
+});
+ 
+app.Run();
