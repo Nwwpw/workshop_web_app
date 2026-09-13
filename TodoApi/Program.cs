9b37add3bc1df26880c711f8b50d455e1c6c4463
Nwwpw <wipawin.t@ku.th>
2026-09-13 15:35:08 +0700

    [fix] fix TodoItem Id type and nullable properties

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+ 
 using TodoApi.Dtos;
+using TodoApi.Models;
+using TodoApi.Data;
  
 var builder = WebApplication.CreateBuilder(args);
  
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+ 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+));
  
 var app = builder.Build();
  
@@ -16,30 +25,109 @@ if (app.Environment.IsDevelopment())
  
 app.UseHttpsRedirection();
  
-var todos = new List<TodoGetDto>
-{
-  new(1, "Learn C#", true),
-  new(2, "Lern ASP .NET Core", false),
-  new(3, "Build a web API", false),
-};
+var todoGroup = app.MapGroup("/api/todos").WithTags("Todos");
  
-app.MapGet("/api/todos", ()=> Results.Ok(todos));
+#region In-Memory Endpoints
  
-app.MapGet("/api/todoes/{id}", (int id) =>
+// var todos = new List<TodoGetDto>
+// {
+//     new(1, "Learn C#", true),
+//     new(2, "Learn ASP.NET Core", false),
+//     new(3, "Build a web API", false)
+// };
+ 
+// todoGroup.MapGet("/", () => Results.Ok(todos));
+ 
+// todoGroup.MapGet("/{id}", (int id) =>
+// {
+//     var todo = todos.FirstOrDefault(t => t.Id == id);
+ 
+//     return todo is not null ? Results.Ok(todo) : Results.NotFound();
+// });
+ 
+// todoGroup.MapPost("/", (TodoPostDto dto) =>
+// {
+//     var nextId = todos.Count == 0 ? 1 : todos.Max(t => t.Id) + 1;
+ 
+//     var todo = new TodoGetDto(nextId, dto.Title, false);
+//     todos.Add(todo);
+ 
+//     return Results.Created($"/{todo.Id}", todo);
+// });
+ 
+// todoGroup.MapPut("/{id}", (int id, TodoPutDto dto) =>
+// {
+//     try
+//     {
+//         var index = todos.FindIndex(t => t.Id == id);
+//         //if (index == -1) return Results.NotFound();
+ 
+//         todos[index] = todos[index] with
+//         {
+//             Title = dto.Title,
+//             IsCompleted = dto.IsCompleted
+//         };
+ 
+//         return Results.Ok(todos[index]);
+//     }
+//     catch (Exception ex)
+//     {
+//         return Results.Problem(ex.Message);
+//     }
+// });
+ 
+// todoGroup.MapDelete("/{id}", (int id) =>
+// {
+//     try
+//     {
+//         var todo = todos.FirstOrDefault(t => t.Id == id);
+//         if (todo is null) return Results.NotFound();
+ 
+//         todos.Remove(todo);
+//         return Results.NoContent();
+//     }
+//     catch(ArgumentNullException ex)
+//     {
+//         return Results.Problem("Parameter is null.{ex.Message}");
+//     }
+//     catch(Exception ex)
+//     {
+//         return Results.Problem(ex.Message);
+//     }
+// });
+ 
+#endregion
+ 
+#region Database Endpoints
+ 
+todoGroup.MapGet("/", async (AppDbContext db) =>
 {
-    var todo = todos.FirstOrDefault(t => t.Id == id);
-
-    return todo is not null ? Results.Ok(todo) : Results.NotFound();
+   var todos = await db.Todos.ToListAsync();
+ 
+   return todos.Count == 0 ? Results.NotFound() : Results.Ok(todos);
 });
-
-app.MapPost("/api/todos", (TodoPostDto dto) =>
-{
-    var nextId = todos.Count == 0 ? 1 : todos.Max(t => t.Id) + 1;
-
-    var todo = new TodoGetDto(nextId, dto.Title, false);
-    todos.Add(todo);
-
-    return Results.Created($"/api/todos/{todo.Id}", todo);
+ 
+todoGroup.MapPost("/", async (AppDbContext db, TodoPostDto dto) =>
+{    
+    var lastTodo = await db.Todos.OrderByDescending(t => t.Id).FirstOrDefaultAsync();
+    var nextId = lastTodo is null ? 1 : lastTodo.Id + 1;
+ 
+    var todo = new TodoItem
+    {
+        Id = nextId,
+        Title = dto.Title,
+        IsCompleted = false,
+        CreatedAt = DateTime.UtcNow
+    };
+ 
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+ 
+    var todoGetDto = new TodoGetDto(todo.Id, todo.Title, todo.IsCompleted);
+ 
+    return Results.Created($"/{todo.Id}", todoGetDto);
 });
+ 
+#endregion
  
 app.Run();
